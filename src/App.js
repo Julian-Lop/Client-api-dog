3b08bd59ae2372917e8aeddeaaca480b2559250b
Julian-Lop <you@example.com>
2022-02-05 10:17:48 -0500

    completado 24

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     
         <Routes>
-          <Route path="/" element={<Landingpage/>}/>
+          <Route path="" element={<Landingpage/>}/>
           <Route path="/home" element={<Homepage/>}/>
           <Route path="/dogDetail/:idRaza" element={<Dogdetail/>}/>
           <Route path="/createDog" element={<Createdog/>}/>

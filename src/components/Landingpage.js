488e7451457a50c0e6b2f5d9485a39949d1d2edf
Julian-Lop <you@example.com>
2022-02-05 15:09:24 -0500

    completado 36

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -15,14 +15,14 @@ function Landingpage(){
         <div className='Landingpage'>
             <div className='glass-left'>
                 <h1>PROYECTO</h1>
-                <h1>INDIVi</h1>
+                <h1>INDIVI</h1>
             </div>
             <div className='transparent-right'>
                 <h1 className='invisible'></h1>
                 <h1>DUAL</h1>
                 <h1>DOGS</h1>
                 <div className='contenedor'>
-                    <Link to='Client-api-dog/home'><button>home</button></Link>
+                    <Link to='/home'><button>home</button></Link>
                 </div> 
             </div>
         </div>

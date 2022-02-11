4dfc221ac81af6f1d5cd6453fad0bcc01df15430
Julian-Lop <you@example.com>
2022-02-11 14:33:51 -0500

    deploy client api peticion correcta

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,7 +13,7 @@ export function getAllDogs(dogname){
     return async function(dispatch){
         try {
             if(dogname){
-                return fetch(`postgres://nhoyqfqswozanj:4e7e1a26588fb3e3d16f25ff0417f020316d58f58d88396dcf7a6ec582a523d3@ec2-34-194-171-47.compute-1.amazonaws.com:5432/dagfmgt132l44e/dogs?name=${dogname}`)
+                return fetch(`https://backendog.herokuapp.com//dogs?name=${dogname}`)
                 .then(response => response.json())
                 .then(json => {
                    return dispatch({type: OBTENER_DOGS, payload: json})
@@ -21,7 +21,7 @@ export function getAllDogs(dogname){
                     return dispatch({type: OBTENER_DOGS, payload: err})
                 })
             }else{
-                let json = await fetch(`postgres://nhoyqfqswozanj:4e7e1a26588fb3e3d16f25ff0417f020316d58f58d88396dcf7a6ec582a523d3@ec2-34-194-171-47.compute-1.amazonaws.com:5432/dagfmgt132l44e/dogs`)
+                let json = await fetch(`https://backendog.herokuapp.com//dogs`)
                 .then(response => response.json())
                 return dispatch({type: OBTENER_DOGS, payload: json})
             }
@@ -33,7 +33,7 @@ export function getAllDogs(dogname){
 
 export function getTemperaments(){
     return async function(dispatch){
-        let json = await fetch('postgres://nhoyqfqswozanj:4e7e1a26588fb3e3d16f25ff0417f020316d58f58d88396dcf7a6ec582a523d3@ec2-34-194-171-47.compute-1.amazonaws.com:5432/dagfmgt132l44e/temperament')
+        let json = await fetch('https://backendog.herokuapp.com//temperament')
         .then(response => response.json())
         return dispatch({type: OBTENER_TEMPERAMENTOS, payload: json})
     }
@@ -43,7 +43,7 @@ export function getDetail(idRaza){
     return function(dispatch){
         try {
             if(idRaza){
-                return fetch(`postgres://nhoyqfqswozanj:4e7e1a26588fb3e3d16f25ff0417f020316d58f58d88396dcf7a6ec582a523d3@ec2-34-194-171-47.compute-1.amazonaws.com:5432/dagfmgt132l44e/dogs/${idRaza}`)
+                return fetch(`https://backendog.herokuapp.com//dogs/${idRaza}`)
                 .then(response => response.json())
                 .then(json => {
                     dispatch({type: OBTENER_INFO_RAZA, payload: json})
@@ -77,7 +77,7 @@ export function organizeByAlphabet(opcionAlphabet){
 
 export function createDog(attributesDog){
     return async function(dispatch){
-        let createdDog = await axios.post(`postgres://nhoyqfqswozanj:4e7e1a26588fb3e3d16f25ff0417f020316d58f58d88396dcf7a6ec582a523d3@ec2-34-194-171-47.compute-1.amazonaws.com:5432/dagfmgt132l44e/dog`,attributesDog)
+        let createdDog = await axios.post(`https://backendog.herokuapp.com//dog`,attributesDog)
         console.log(createDog)
         return createdDog
     } 
